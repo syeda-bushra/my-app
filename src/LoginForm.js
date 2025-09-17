89443b5649f32796f791f57b28bfc3f0e81b54dd
Syeda Bushra <bushra@dynasoftcloud.com>
2025-09-17 16:50:51 +0500

    Login Form

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,16 +1,21 @@
 import './App.css';
-
 function LoginForm() {
   const loginForm = document.getElementById("loginForm");
   loginForm.addEventListener('submit', function(event){
     event.preventDefault();
 
+    const name = document.getElementById('email').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-
-    console.log("Email:",email)
-    console.log("Password:",password)
-    alert(`Logging in with:\nEmail: ${email}\nPassword: ${'*'.repeat(password.length)}`);
+    
+    const user = {
+        name: name,
+        password: password,
+        email: email
+      };
+    console.log("user",user)
+    alert(`Logging in with:\nname: ${name}\nEmail: ${email}\nPassword: ${'*'.repeat(password.length)}`);
+   
   } )
 }
 
