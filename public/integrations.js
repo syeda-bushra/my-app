18db53519d7eb7a4b7231de45bf83f3f76f5b0ec
Syeda Bushra <bushra@dynasoftcloud.com>
2025-09-25 16:45:26 +0500

    Changed page names and created a Contact page

--- public/integrations.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const name = localStorage.getItem('loggedInUserName');
-    const display = document.getElementById('userNameDisplay');
-
-    if (name && display) {
-        display.textContent = name;
-    } else {
-        display.textContent = 'Guest';}
-    });
